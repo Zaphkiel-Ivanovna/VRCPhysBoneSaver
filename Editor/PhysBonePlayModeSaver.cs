154c5d90e72119e6ad0193531ab7fadd175aa473
Zaphkiel <duambi123@gmail.com>
2023-10-31 22:23:13 +0100

    🚀 Init Push

---
+++ Editor/PhysBonePlayModeSaver.cs
@@ -0,0 +1,74 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using VRC.SDK3.Dynamics.PhysBone.Components;
+
+namespace VRCPhysBoneSaver.Editor
+{
+    [InitializeOnLoad]
+    public static class PhysBonePlayModeSaver
+    {
+        private const string PREFERENCE_KEY = "VRCPhysBoneSaver.EnablePlayModeSave";
+        private const string KEY_PREFIX = "ScenePresetManagement";
+
+        static PhysBonePlayModeSaver()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange playModeState)
+        {
+            if (!EditorPrefs.GetBool(PREFERENCE_KEY, true)) return;
+
+            switch (playModeState)
+            {
+                case PlayModeStateChange.EnteredEditMode:
+                    LoadPhysBoneComponents();
+                    break;
+                case PlayModeStateChange.ExitingPlayMode:
+                    SavePhysBoneComponents();
+                    break;
+            }
+        }
+
+        private static void LoadPhysBoneComponents()
+        {
+            LoadComponentState<VRCPhysBone>();
+            LoadComponentState<VRCPhysBoneCollider>();
+            EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+        }
+
+        private static void SavePhysBoneComponents()
+        {
+            SaveComponentState<VRCPhysBone>();
+            SaveComponentState<VRCPhysBoneCollider>();
+        }
+
+        private static void LoadComponentState<T>() where T : Component
+        {
+            var components = Object.FindObjectsOfType<T>();
+            foreach (var component in components)
+            {
+                var prefKey = $"{KEY_PREFIX}.{component.GetInstanceID()}";
+                if (!EditorPrefs.HasKey(prefKey)) continue;
+
+                var componentData = EditorPrefs.GetString(prefKey);
+                JsonUtility.FromJsonOverwrite(componentData, component);
+
+                EditorPrefs.DeleteKey(prefKey);
+                EditorUtility.SetDirty(component);
+            }
+        }
+
+        private static void SaveComponentState<T>() where T : Component
+        {
+            var components = Object.FindObjectsOfType<T>();
+            foreach (var component in components)
+            {
+                var prefKey = $"{KEY_PREFIX}.{component.GetInstanceID()}";
+                var componentData = JsonUtility.ToJson(component);
+                EditorPrefs.SetString(prefKey, componentData);
+            }
+        }
+    }
+}
---
+++ Editor/ProjectSettingsRegister.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace VRCPhysBoneSaver.Editor
+{
+    public static class PhysBoneSaverSettings
+    {
+        private const string ENABLE_PLAYMODE_SAVE_KEY = "VRCPhysBoneSaver.EnablePlayModeSave";
+
+        [SettingsProvider]
+        public static SettingsProvider CreateSettingsProvider()
+        {
+            var provider = new SettingsProvider("Preferences/VRCPhysBoneSaver", SettingsScope.User)
+            {
+                label = "VRC PhysBone Saver",
+                guiHandler = DrawSettingsInterface,
+                keywords = new HashSet<string>(new[] { "VRC", "PhysBone", "Saver", "Settings" })
+            };
+
+            return provider;
+        }
+
+        private static void DrawSettingsInterface(string searchContext)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(10); // Adjust this value to your desired spacing
+            bool enablePlayModeSave = EditorPrefs.GetBool(ENABLE_PLAYMODE_SAVE_KEY, true);
+            bool newEnablePlayModeSave = EditorGUILayout.Toggle("Enable PlayMode Save", enablePlayModeSave);
+            GUILayout.EndHorizontal();
+
+            if (enablePlayModeSave != newEnablePlayModeSave)
+            {
+                EditorPrefs.SetBool(ENABLE_PLAYMODE_SAVE_KEY, newEnablePlayModeSave);
+            }
+
+            using (new GUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button("Made By Zaphkiel", "boldlabel"))
+                    Application.OpenURL("https://github.com/Zaphkiel-Ivanovna");
+            }
+        }
+
+    }
+}
